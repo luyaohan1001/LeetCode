0522cff4af1f3b73450bae2d88c74950c7e05f38
Luyao Han <lhan@luyaosmac.attlocal.net>
2024-04-30 22:09:06 -0700

    Added 55-Jump-Game with Greedy Algorithm

---
+++ 55-jump-game/CMakeFiles/jump-game.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jump-game.dir/src/main.cpp.o"
+  "CMakeFiles/jump-game.dir/src/main.cpp.o.d"
+  "jump-game"
+  "jump-game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jump-game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 55-jump-game/CMakeFiles/jump-game.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for jump-game.
+# This may be replaced when dependencies are built.
